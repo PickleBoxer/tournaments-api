3e8e2bdf01c8beef28251848d1c727112e47d345
Matic Vertačnik <matic.vertacnik@icloud.com>
2025-11-03 22:02:44 +0100

    Add Scramble configuration and implement OpenAPI generation logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Dedoc\Scramble\Scramble;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Scramble::afterOpenApiGenerated(function (\Dedoc\Scramble\Support\Generator\OpenApi $openApi) {
+            $openApi->components->securitySchemes = [];
+            $openApi->security = [];
+        });
     }
 }
